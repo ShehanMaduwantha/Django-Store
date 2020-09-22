d0792181c84fc6084d4fbd864fa076f5af763d30
toraya-eng <63116236+toraya-eng@users.noreply.github.com>
2020-09-22 12:27:01 +0100

    Add files via upload

---
+++ store/urls.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.store, name="store"),
+    path('cart/', views.cart, name="cart"),
+    path('checkout/', views.checkout, name="checkout")
+]
